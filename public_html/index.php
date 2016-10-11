2e13617b8dc5adef8315cd4d3a1af4198253d588
Hector Lozano <hlozano2@cnm.edu>
2016-10-11 15:52:38 -0600

    Added HTML

--- public_html/index.php
+++ public_html/index.php
@@ -2,6 +2,9 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8"/>
+
+		<!-- add CSS file -->
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css"/>
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
@@ -18,11 +21,11 @@
 
 			<section>
 				<p>
-					Lucas ipsum dolor sit amet rahn nar binks bibble b4-d4 calrissian utapau quarren snivvian dak. Mustafar sly ogemite firrerreo firmus. Rattatak dengar daala jarael typho. Han darth cliegg jerec. Bothan kamino ki-adi-mundi aparo coruscant letaki. Yowza c-3po hapes airen snootles duro kanos. Wicket amedda devaronian di organa rhen. Galen winter hutt nomi. Xanatos calamari zuckuss glymphid er'kit weequay. Baba rahm gerb polis sio besalisk hutt. Geonosis shimrra nute kamino b4-d4 bibble bothawui. Zabrak bando sulorine boba.
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel nisi eu urna molestie tristique vel nec augue. Ut vulputate nisl ut justo molestie, eget eleifend nibh vestibulum. Nulla lorem augue, cursus a pretium a, facilisis in orci. Curabitur ac justo fermentum, varius metus non, lobortis felis. In hac habitasse platea dictumst. Cras tristique rutrum nisi, vitae semper lacus maximus a. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam blandit non leo non tincidunt. Morbi convallis dolor et mauris sagittis maximus. Vestibulum scelerisque facilisis tellus a euismod. Phasellus ex tellus, dignissim sed eros fermentum, aliquam aliquet arcu. Duis tincidunt mauris at tellus scelerisque commodo. In a nisi urna.
 
-					Obi-wan artaru kaminoan zannah kel hutta mon whaladon. Droopy gotal nomi wessell corran arvel wicket jin'ha. Kael luuke oppo phlog di chazrach tarkin. Oola amidala joelle troig jabba rom hypori t'landa metalorn. Naberrie yoda glymphid mothma. H'nemthean ruurian piett gorog dunhausen boz. Polis arvel antilles moff bane felth gricho. Illum nadd organa typho nadon durge frozarns vong hutt. Elom qu ansuroer solo maul moff fode. Gen'dai caedus letaki ziro sio disra rahn. Abrion offee vurk vel thennqora tatooine whitesun.
+					Proin purus sapien, rhoncus nec dapibus eu, facilisis et ligula. Aliquam eget ipsum laoreet, consectetur neque a, lacinia lectus. Nullam posuere diam at nulla molestie elementum. Integer bibendum, quam aliquam vestibulum feugiat, ante quam porttitor diam, eget feugiat neque metus eu sem. Praesent egestas maximus suscipit. Curabitur maximus orci sit amet elit scelerisque ultrices. Nunc nisi arcu, vulputate sed augue ac, maximus dignissim ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lorem nisl, semper vel commodo eu, feugiat in leo. Nullam commodo sagittis nisl nec ornare. Curabitur hendrerit pellentesque pulvinar.
 
-					Unduli yuvernian tiin vos zuggs. Carlist lumiya saurin wedge aparo ansion grievous. Kota sifo-dyas typho vos kota teneniel karrde cadavine pau'an. Echani pavan bertroff darth tapani whiphid porkins katarn jerjerrod. Mimbanite gand snootles charal cadavine dooku verpine offee kyle. Hobbie sal-solo xizor kenobi selonian vong cabasshite -1b. Biggs hoojib naboo annoo watto jusik. Jin'ha gricho nelvaanian tenel fisto -lom maul raioballo defel. Allie kor-uj billaba yowza weequay iblis dashade moff. Vedder aurra sifo-dyas mirialan tyranus wol fode.
+					Donec id magna pretium, fermentum nisl a, euismod ante. Nullam lectus felis, posuere eget pulvinar eu, blandit eu nulla. Proin consectetur mollis ex, sit amet finibus neque malesuada et. Aenean vel pharetra metus, vitae porttitor sapien. Vestibulum non posuere mauris, non imperdiet tortor. Curabitur tempus lacus eget tortor vehicula dignissim. Phasellus vitae tellus ac est gravida vehicula. Duis a molestie nunc. Etiam at odio nunc.
 				</p>
 					<img src="img/femninja.tiff" alt="femninja">
 			</section>
@@ -34,7 +37,7 @@
 					<li>Senator Arlo</li>
 					<li>Star Wars</li>
 					<li>Beer</li>
-					<li><Donuts/li>
+					<li>Donuts</li>
 				</ul>
 			</section>
 
