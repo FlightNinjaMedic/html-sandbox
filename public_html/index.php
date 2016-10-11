ac3e6b09395204ef36dd2cf20b7a757eca2247de
Hector Lozano <hlozano2@cnm.edu>
2016-10-11 13:46:32 -0600

    Added HTML

--- public_html/index.php
+++ public_html/index.php
@@ -1,10 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8">
+		<meta charset="UTF-8"/>
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
-		<h1>Hello World! </h1>
+
+		<!-- this is the header -->
+		<header>
+			<h1>Hi! This is my page.</h1>
+		</header>
+
+		<!-- this is the main page content -->
+		<main>
+
+			<!-- begin section 1 -->
+
+			<section>
+				<p>
+					Lucas ipsum dolor sit amet rahn nar binks bibble b4-d4 calrissian utapau quarren snivvian dak. Mustafar sly ogemite firrerreo firmus. Rattatak dengar daala jarael typho. Han darth cliegg jerec. Bothan kamino ki-adi-mundi aparo coruscant letaki. Yowza c-3po hapes airen snootles duro kanos. Wicket amedda devaronian di organa rhen. Galen winter hutt nomi. Xanatos calamari zuckuss glymphid er'kit weequay. Baba rahm gerb polis sio besalisk hutt. Geonosis shimrra nute kamino b4-d4 bibble bothawui. Zabrak bando sulorine boba.
+
+					Obi-wan artaru kaminoan zannah kel hutta mon whaladon. Droopy gotal nomi wessell corran arvel wicket jin'ha. Kael luuke oppo phlog di chazrach tarkin. Oola amidala joelle troig jabba rom hypori t'landa metalorn. Naberrie yoda glymphid mothma. H'nemthean ruurian piett gorog dunhausen boz. Polis arvel antilles moff bane felth gricho. Illum nadd organa typho nadon durge frozarns vong hutt. Elom qu ansuroer solo maul moff fode. Gen'dai caedus letaki ziro sio disra rahn. Abrion offee vurk vel thennqora tatooine whitesun.
+
+					Unduli yuvernian tiin vos zuggs. Carlist lumiya saurin wedge aparo ansion grievous. Kota sifo-dyas typho vos kota teneniel karrde cadavine pau'an. Echani pavan bertroff darth tapani whiphid porkins katarn jerjerrod. Mimbanite gand snootles charal cadavine dooku verpine offee kyle. Hobbie sal-solo xizor kenobi selonian vong cabasshite -1b. Biggs hoojib naboo annoo watto jusik. Jin'ha gricho nelvaanian tenel fisto -lom maul raioballo defel. Allie kor-uj billaba yowza weequay iblis dashade moff. Vedder aurra sifo-dyas mirialan tyranus wol fode.
+				</p>
+					<img src="img/femninja.tiff" alt="femninja">
+			</section>
+
+			<!-- begin section 2 -->
+			<section>
+				<h2></h2>
+				<ul>
+					<li>Senator Arlo</li>
+					<li>Star Wars</li>
+					<li>Beer</li>
+					<li><Donuts/li>
+				</ul>
+			</section>
+
+			<!-- begin section 3-->
+			<section>
+
+			</section>
+		</main>
 	</body>
 </html>
